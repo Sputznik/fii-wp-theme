8fe7ba581eef4636af94490ead69631db7e8aef1
psychome004 <anil.stephen66@gmail.com>
2022-06-24 16:04:51 +0530

    Theme support

--- functions.php
+++ functions.php
@@ -37,6 +37,8 @@ add_action( 'init', function(){
 
 add_action( 'after_setup_theme', function() {
 	add_theme_support( 'post-thumbnails' );
+  add_theme_support( 'wp-block-styles' );
+  add_theme_support( 'responsive-embeds' );
 });
 
 /* ADD SOW FROM THE THEME */
