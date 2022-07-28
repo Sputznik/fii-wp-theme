6bb013a8a9c9b2ae4ca3b05b50a9b14c452718be
psychome004 <anil.stephen66@gmail.com>
2022-07-28 13:48:16 +0530

    Add archive template

---
+++ archive.php
@@ -0,0 +1,27 @@
+<?php
+/**
+ * The template for displaying archive pages
+ */
+get_header();
+
+$date_query = '';
+$archive_query = $wp_query->query;
+if( is_day()  ) {
+  $date_query = 'year:'.$archive_query['year'].'ANDmonth:'.$archive_query['monthnum'].'ANDday:'.$archive_query['day'];
+} elseif( is_month() ) {
+  $date_query = 'year:'.$archive_query['year'].'ANDmonth:'.$archive_query['monthnum'];
+} elseif( is_year() ){
+  $date_query = 'year:'.$archive_query['year'];
+}
+
+?>
+<div class="container archive-page">
+  <h1 class="page-title">Archive</h1>
+  <div class="orbit-posts-wrapper">
+    <?php echo do_shortcode("[orbit_query posts_per_page='9' style='grid3' date_query='".$date_query."' pagination='1' ]"); ?>
+  </div>
+  <div class="archive-sidebar">
+    <?php if ( is_active_sidebar( 'fii-category-sidebar' ) ) { dynamic_sidebar( 'fii-category-sidebar' ); } ?>
+  </div>
+</div>
+<?php get_footer(); ?>
