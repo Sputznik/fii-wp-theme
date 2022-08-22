ab0c55f236f2e06687eb7a9901711e3b360ca249
psychome004 <anil.stephen66@gmail.com>
2022-08-22 15:02:22 +0530

    Add 404 template

---
+++ 404.php
@@ -0,0 +1,18 @@
+<?php
+/**
+ * The template for displaying 404 pages (not found)
+ */
+get_header();
+?>
+<div class="container wrapper-margin">
+  <div class="error-header">
+    <h1>Ooops… Error 404</h1>
+    <p>Sorry, but the page you are looking for doesn’t exist.</p>
+    <?php echo do_shortcode("[fii_button text='Go To Homepage' url='".get_bloginfo('url')."' align='center']"); ?>
+  </div>
+  <div class="orbit-posts-wrapper">
+    <h2>Latest Posts</h2>
+    <?php echo do_shortcode("[orbit_query posts_per_page='6' style='grid3']"); ?>
+  </div>
+</div>
+<?php get_footer(); ?>
