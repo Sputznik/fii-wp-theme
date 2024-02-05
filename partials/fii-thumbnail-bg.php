ab72f28c6c77ff2e1491555b4ceeb22057dcb14d
psychome004 <anil.stephen66@gmail.com>
2024-02-05 11:56:28 +0530

    Update theme assets version

--- partials/fii-thumbnail-bg.php
+++ partials/fii-thumbnail-bg.php
@@ -1,5 +1,5 @@
 <?php
-  $thumbnail = get_the_post_thumbnail( $post->ID, 'full', array( 'alt'=> 'Featured Image', 'style' => 'display:block;' ) );
+  $thumbnail = get_the_post_thumbnail( $post->ID, 'full', array( 'alt'=> 'Featured Image' ) );
 ?>
 <a href="<?php _e( get_the_permalink() );?>" class="fii-fluid-thumbnail">
   <?php if( $thumbnail ): echo $thumbnail; ?>
